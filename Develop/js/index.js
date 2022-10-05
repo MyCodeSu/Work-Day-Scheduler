198f9c1300457da46d9fb2cd14133da5c98fcf7c
michaelsu01@gmail.com <michaelsu01@gmail.com>
2022-10-05 04:44:54 -0400

    changed time block text colour to black

--- Develop/js/index.js
+++ Develop/js/index.js
@@ -4,13 +4,13 @@
 // WHEN I scroll down
 // THEN I am presented with time blocks for standard business hours                                     DONE
 // WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
+// THEN each time block is color-coded to indicate whether it is in the past, present, or future        DONE
 // WHEN I click into a time block
 // THEN I can enter an event                                                                            Done
 // WHEN I click the save button for that time block
-// THEN the text for that event is saved in local storage
+// THEN the text for that event is saved in local storage                                               DONE
 // WHEN I refresh the page
-// THEN the saved events persist
+// THEN the saved events persist                                                                        DONE
 
 const currentDate = moment().format('MMMM Do, YYYY');
 const currentDayContainer = document.getElementById("currentDay");
